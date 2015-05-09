fixed off-by-one prediction length

pyESN.py
@@ -232,9 +232,10 @@ class ESN():
         states = np.vstack([laststate, np.zeros((n_samples, self.n_reservoir))])
         outputs = np.vstack([lastoutput,np.zeros((n_samples, self.n_outputs))])
 
-        for n in range(1,n_samples):
-            states[n,:] = self._update(states[n-1,:],inputs[n,:],outputs[n-1,:])
-            outputs[n,:] = self.out_activation(np.dot(self.W_out,
-                                    np.concatenate([states[n,:],inputs[n,:]])))
+        for n in range(n_samples):
+            states[n+1,:] = self._update(states[n,:],inputs[n+1,:],outputs[n,:])
+            outputs[n+1,:] = self.out_activation(np.dot(self.W_out,
+                                    np.concatenate([states[n+1,:],inputs[n+1,:]])))
+
 
         return self._unscale_teacher(self.out_activation(outputs[1:]))

testing.py
@@ -143,7 +143,7 @@ class Performance(unittest.TestCase):
         esn.fit(np.ones(trainlen),data[:trainlen])
         prediction = esn.predict(np.ones(future))
         error = np.sqrt(np.mean((prediction.flatten() - data[trainlen:trainlen+future])**2))
-        self.assertAlmostEqual(error,0.139607171323)
+        self.assertAlmostEqual(error,0.1396039098653574)
 
     def test_freqgen(self):
         rng = np.random.RandomState(42)
@@ -197,7 +197,7 @@ class Performance(unittest.TestCase):
         #print "test error:"
         pred_test = esn.predict(test_ctrl)
         error = np.sqrt(np.mean((pred_test - test_output)**2))
-        self.assertAlmostEqual(error,0.390424480706)
+        self.assertAlmostEqual(error,0.39037112433756577)
 
 
 if __name__ == '__main__':