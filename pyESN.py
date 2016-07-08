e16c95cbf31fcb3d2c5703cc93afdd3bf6bead6d
cknd <ck-github@erleuchtet.org>
2016-07-08 18:54:10 +0200

    py 3 & pep8

--- pyESN.py
+++ pyESN.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def correct_dimensions(s,targetlength):
+
+def correct_dimensions(s, targetlength):
     """checks the dimensionality of some numeric argument s, broadcasts it
        to the specified length if possible.
 
@@ -14,22 +15,23 @@ def correct_dimensions(s,targetlength):
     if s is not None:
         s = np.array(s)
         if s.ndim == 0:
-            s = np.array([s]*targetlength)
+            s = np.array([s] * targetlength)
         elif s.ndim == 1:
             if not len(s) == targetlength:
-                raise ValueError("arg must have length "+str(targetlength))
+                raise ValueError("arg must have length " + str(targetlength))
         else:
             raise ValueError("Invalid argument")
     return s
 
-class ESN():
-    def __init__(self, n_inputs, n_outputs, n_reservoir=200,
-                 spectral_radius=0.95, sparsity=0, noise=0.001,input_shift=None,
-                 input_scaling=None, teacher_forcing=True,feedback_scaling=None,
-                 teacher_scaling=None, teacher_shift=None,
-                 out_activation=lambda x:x, inverse_out_activation=lambda x:x,
-                 random_state=None,silent=True):
 
+class ESN():
+
+    def __init__(self, n_inputs, n_outputs, n_reservoir=200,
+                 spectral_radius=0.95, sparsity=0, noise=0.001, input_shift=None,
+                 input_scaling=None, teacher_forcing=True, feedback_scaling=None,
+                 teacher_scaling=None, teacher_shift=None,
+                 out_activation=lambda x: x, inverse_out_activation=lambda x: x,
+                 random_state=None, silent=True):
         """
         Args:
             n_inputs: nr of input dimensions
@@ -52,18 +54,17 @@ class ESN():
             silent: supress messages
         """
         # check for proper dimensionality of all arguments and write them down.
-        self.n_inputs = correct_dimensions(n_inputs,1)
-        self.n_reservoir = correct_dimensions(n_reservoir,1)
-        self.n_outputs = correct_dimensions(n_outputs,1)
-        self.spectral_radius = correct_dimensions(spectral_radius,1)
-        self.sparsity = correct_dimensions(sparsity,1)
-        self.noise = correct_dimensions(noise,1)
-        self.noise_ = self.noise
-        self.input_shift = correct_dimensions(input_shift,n_inputs)
-        self.input_scaling = correct_dimensions(input_scaling,n_inputs)
+        self.n_inputs = n_inputs
+        self.n_reservoir = n_reservoir
+        self.n_outputs = n_outputs
+        self.spectral_radius = spectral_radius
+        self.sparsity = sparsity
+        self.noise = noise
+        self.input_shift = correct_dimensions(input_shift, n_inputs)
+        self.input_scaling = correct_dimensions(input_scaling, n_inputs)
 
-        self.teacher_scaling = correct_dimensions(teacher_scaling,1)
-        self.teacher_shift = correct_dimensions(teacher_shift,1)
+        self.teacher_scaling = teacher_scaling
+        self.teacher_shift = teacher_shift
 
         self.out_activation = out_activation
         self.inverse_out_activation = inverse_out_activation
@@ -77,7 +78,7 @@ class ESN():
             try:
                 self.random_state_ = np.random.RandomState(random_state)
             except TypeError as e:
-                raise Exception("Invalid seed: "+str(e))
+                raise Exception("Invalid seed: " + str(e))
         else:
             self.random_state_ = np.random.mtrand._rand
 
@@ -88,46 +89,48 @@ class ESN():
     def initweights(self):
         # initialize recurrent weights:
         # begin with a random matrix centered around zero:
-        W = self.random_state_.rand(self.n_reservoir,self.n_reservoir) - 0.5
+        W = self.random_state_.rand(self.n_reservoir, self.n_reservoir) - 0.5
         # delete the fraction of connections given by (self.sparsity):
         W[self.random_state_.rand(*W.shape) < self.sparsity] = 0
         # compute the spectral radius of these weights:
         radius = np.max(np.abs(np.linalg.eigvals(W)))
         # rescale them to reach the requested spectral radius:
-        self.W = W*(self.spectral_radius/radius)
+        self.W = W * (self.spectral_radius / radius)
 
         # random input weights:
-        self.W_in = self.random_state_.rand(self.n_reservoir, self.n_inputs)*2-1
+        self.W_in = self.random_state_.rand(
+            self.n_reservoir, self.n_inputs) * 2 - 1
         # random feedback (teacher forcing) weights:
-        self.W_feedb = self.random_state_.rand(self.n_reservoir, self.n_outputs)*2-1
+        self.W_feedb = self.random_state_.rand(
+            self.n_reservoir, self.n_outputs) * 2 - 1
 
-    def _update(self,state,input_pattern,output_pattern):
+    def _update(self, state, input_pattern, output_pattern):
         """performs one update step.
 
         i.e., computes the next network state by applying the recurrent weights
         to the last state & and feeding in the current input and output patterns
         """
         if self.teacher_forcing:
-            preactivation = (np.dot(self.W,state)
-                            + np.dot(self.W_in,input_pattern)
-                            + np.dot(self.W_feedb,output_pattern))
+            preactivation = (np.dot(self.W, state)
+                             + np.dot(self.W_in, input_pattern)
+                             + np.dot(self.W_feedb, output_pattern))
         else:
-            preactivation = (np.dot(self.W,state)
-                            + np.dot(self.W_in,input_pattern))
+            preactivation = (np.dot(self.W, state)
+                             + np.dot(self.W_in, input_pattern))
         return (np.tanh(preactivation)
-               + self.noise*(self.random_state_.rand(self.n_reservoir)-0.5))
+                + self.noise * (self.random_state_.rand(self.n_reservoir) - 0.5))
 
-    def _scale_inputs(self,inputs):
+    def _scale_inputs(self, inputs):
         """for each input dimension j: multiplies by the j'th entry in the
         input_scaling argument, then adds the j'th entry of the input_shift
         argument."""
         if self.input_scaling is not None:
-            inputs = np.dot(inputs,np.diag(self.input_scaling))
+            inputs = np.dot(inputs, np.diag(self.input_scaling))
         if self.input_shift is not None:
             inputs = inputs + self.input_shift
         return inputs
 
-    def _scale_teacher(self,teacher):
+    def _scale_teacher(self, teacher):
         """multiplies the teacher/target signal by the teacher_scaling argument,
         then adds the teacher_shift argument to it."""
         if self.teacher_scaling is not None:
@@ -136,7 +139,7 @@ class ESN():
             teacher = teacher + self.teacher_shift
         return teacher
 
-    def _unscale_teacher(self,teacher_scaled):
+    def _unscale_teacher(self, teacher_scaled):
         """inverse operation of the _scale_teacher method."""
         if self.teacher_shift is not None:
             teacher_scaled = teacher_scaled - self.teacher_shift
@@ -144,7 +147,7 @@ class ESN():
             teacher_scaled = teacher_scaled / self.teacher_scaling
         return teacher_scaled
 
-    def fit(self,inputs,outputs,inspect=False):
+    def fit(self, inputs, outputs, inspect=False):
         """
         Collect the network's reaction to training data, train readout weights.
 
@@ -158,53 +161,58 @@ class ESN():
         """
         # transform any vectors of shape (x,) into vectors of shape (x,1):
         if inputs.ndim < 2:
-            inputs = np.reshape(inputs,(len(inputs),-1))
+            inputs = np.reshape(inputs, (len(inputs), -1))
         if outputs.ndim < 2:
-            outputs = np.reshape(outputs,(len(outputs),-1))
+            outputs = np.reshape(outputs, (len(outputs), -1))
         # transform input and teacher signal:
         inputs_scaled = self._scale_inputs(inputs)
         teachers_scaled = self._scale_teacher(outputs)
 
-        if not self.silent: print("harvesting states...")
+        if not self.silent:
+            print("harvesting states...")
         # step the reservoir through the given input,output pairs:
         states = np.zeros((inputs.shape[0], self.n_reservoir))
-        for n in range(1,inputs.shape[0]):
-            states[n,:] = self._update(states[n-1],inputs_scaled[n,:],
-                                                         teachers_scaled[n-1,:])
+        for n in range(1, inputs.shape[0]):
+            states[n, :] = self._update(states[n - 1], inputs_scaled[n, :],
+                                        teachers_scaled[n - 1, :])
 
         # learn the weights, i.e. find the linear combination of collected
         # network states that is closest to the target output
-        if not self.silent: print("fitting...")
+        if not self.silent:
+            print("fitting...")
         # we'll disregard the first few states:
-        transient = min(int(inputs.shape[1]/10),100)
+        transient = min(int(inputs.shape[1] / 10), 100)
         # include the raw inputs:
-        extended_states = np.hstack((states,inputs_scaled))
+        extended_states = np.hstack((states, inputs_scaled))
         # Solve for W_out:
-        self.W_out = np.dot(np.linalg.pinv(extended_states[transient:,:]),
-                  self.inverse_out_activation(teachers_scaled[transient:,:])).T
+        self.W_out = np.dot(np.linalg.pinv(extended_states[transient:, :]),
+                            self.inverse_out_activation(teachers_scaled[transient:, :])).T
 
         # remember the last state for later:
-        self.laststate = states[-1,:]
-        self.lastinput = inputs[-1,:]
-        self.lastoutput = outputs[-1,:]
+        self.laststate = states[-1, :]
+        self.lastinput = inputs[-1, :]
+        self.lastoutput = outputs[-1, :]
 
         # optionally visualize the collected states
         if inspect:
             from matplotlib import pyplot as plt
             # (^-- we depend on matplotlib only if this option is used)
-            plt.figure(figsize=(states.shape[0]*0.0025,states.shape[1]*0.01))
-            plt.imshow(extended_states.T,aspect='auto',interpolation='nearest')
+            plt.figure(
+                figsize=(states.shape[0] * 0.0025, states.shape[1] * 0.01))
+            plt.imshow(extended_states.T, aspect='auto',
+                       interpolation='nearest')
             plt.colorbar()
 
-        if not self.silent: print("training error:")
+        if not self.silent:
+            print("training error:")
         # apply learned weights to the collected states:
         pred_train = self._unscale_teacher(self.out_activation(
-                                          np.dot(extended_states,self.W_out.T)))
-        if not self.silent: print(np.sqrt(np.mean((pred_train - outputs)**2)))
+            np.dot(extended_states, self.W_out.T)))
+        if not self.silent:
+            print(np.sqrt(np.mean((pred_train - outputs)**2)))
         return pred_train
 
-
-    def predict(self,inputs,continuation=True):
+    def predict(self, inputs, continuation=True):
         """
         Apply the learned weights to the network's reactions to new input.
 
@@ -216,7 +224,7 @@ class ESN():
             Array of output activations
         """
         if inputs.ndim < 2:
-            inputs = np.reshape(inputs,(len(inputs),-1))
+            inputs = np.reshape(inputs, (len(inputs), -1))
         n_samples = inputs.shape[0]
 
         if continuation:
@@ -229,13 +237,15 @@ class ESN():
             lastoutput = np.zeros(self.n_outputs)
 
         inputs = np.vstack([lastinput, self._scale_inputs(inputs)])
-        states = np.vstack([laststate, np.zeros((n_samples, self.n_reservoir))])
-        outputs = np.vstack([lastoutput,np.zeros((n_samples, self.n_outputs))])
+        states = np.vstack(
+            [laststate, np.zeros((n_samples, self.n_reservoir))])
+        outputs = np.vstack(
+            [lastoutput, np.zeros((n_samples, self.n_outputs))])
 
         for n in range(n_samples):
-            states[n+1,:] = self._update(states[n,:],inputs[n+1,:],outputs[n,:])
-            outputs[n+1,:] = self.out_activation(np.dot(self.W_out,
-                                    np.concatenate([states[n+1,:],inputs[n+1,:]])))
-
+            states[
+                n + 1, :] = self._update(states[n, :], inputs[n + 1, :], outputs[n, :])
+            outputs[n + 1, :] = self.out_activation(np.dot(self.W_out,
+                                                           np.concatenate([states[n + 1, :], inputs[n + 1, :]])))
 
         return self._unscale_teacher(self.out_activation(outputs[1:]))
--- testing.py
+++ testing.py
@@ -3,201 +3,217 @@ import numpy as np
 
 from pyESN import ESN
 
-N_in,N,N_out = 5,75,3
+N_in, N, N_out = 5, 75, 3
+
 
 def random_task():
-    X = np.random.randn(100,N_in)
-    y = np.random.randn(100,N_out)
-    Xp = np.random.randn(50,N_in)
-    return X,y,Xp
+    X = np.random.randn(100, N_in)
+    y = np.random.randn(100, N_out)
+    Xp = np.random.randn(50, N_in)
+    return X, y, Xp
 
 
 class RandomStateHandling(unittest.TestCase):
+
     def setUp(self):
         self.task = random_task()
 
-    def _compare(self,esnA,esnB,should_be):
+    def _compare(self, esnA, esnB, should_be):
         """helper function to see if two esns are the same"""
-        X,y,Xp = self.task
-        test = self.assertTrue if should_be=="same" else self.assertFalse
+        X, y, Xp = self.task
+        test = self.assertTrue if should_be == "same" else self.assertFalse
         test(np.all(np.equal(esnA.W, esnB.W)))
         test(np.all(np.equal(esnA.W_in, esnB.W_in)))
         test(np.all(np.equal(esnA.W_feedb, esnB.W_feedb)))
-        test(np.all(np.equal(esnA.fit(X,y), esnB.fit(X,y))))
+        test(np.all(np.equal(esnA.fit(X, y), esnB.fit(X, y))))
         test(np.all(np.equal(esnA.W_out, esnB.W_out)))
         test(np.all(np.equal(esnA.predict(Xp), esnB.predict(Xp))))
 
     def test_integer(self):
         """two esns with the same seed should be the same"""
-        esnA = ESN(N_in,N_out,random_state=1)
-        esnB = ESN(N_in,N_out,random_state=1)
-        self._compare(esnA,esnB,should_be="same")
+        esnA = ESN(N_in, N_out, random_state=1)
+        esnB = ESN(N_in, N_out, random_state=1)
+        self._compare(esnA, esnB, should_be="same")
 
     def test_randomstate_object(self):
         """two esns with the same randomstate objects should be the same"""
         rstA = np.random.RandomState(1)
-        esnA = ESN(N_in,N_out,random_state=rstA)
+        esnA = ESN(N_in, N_out, random_state=rstA)
         rstB = np.random.RandomState(1)
-        esnB = ESN(N_in,N_out,random_state=rstB)
-        self._compare(esnA,esnB,should_be="same")
+        esnB = ESN(N_in, N_out, random_state=rstB)
+        self._compare(esnA, esnB, should_be="same")
 
     def test_none(self):
         """two esns with no specified seed should be different"""
-        esnA = ESN(N_in,N_out,random_state=None)
-        esnB = ESN(N_in,N_out,random_state=None)
-        self._compare(esnA,esnB,should_be="different")
-
+        esnA = ESN(N_in, N_out, random_state=None)
+        esnB = ESN(N_in, N_out, random_state=None)
+        self._compare(esnA, esnB, should_be="different")
 
     def test_nonsense(self):
         """parameter random_state should only accept positive integers"""
         with self.assertRaises(ValueError):
-            ESN(N_in,N_out,random_state=-1)
+            ESN(N_in, N_out, random_state=-1)
 
         with self.assertRaises(Exception) as cm:
-            ESN(N_in,N_out,random_state=0.5)
-        self.assertIn("Invalid seed",str(cm.exception))
+            ESN(N_in, N_out, random_state=0.5)
+        self.assertIn("Invalid seed", str(cm.exception))
 
 
 class InitArguments(unittest.TestCase):
+
     def setUp(self):
-        self.X,self.y,self.Xp = random_task()
+        self.X, self.y, self.Xp = random_task()
 
     def test_inputscaling(self):
         """input scaling factors of different formats should be correctly intereted or rejected"""
-        esn = ESN(N_in,N_out,input_scaling=2)
-        self.assertTrue(np.all(2*self.X == esn._scale_inputs(self.X)))
-        esn.fit(self.X,self.y)
+        esn = ESN(N_in, N_out, input_scaling=2)
+        self.assertTrue(np.all(2 * self.X == esn._scale_inputs(self.X)))
+        esn.fit(self.X, self.y)
         esn.predict(self.Xp)
 
-        esn = ESN(N_in,N_out,input_scaling=[2]*N_in)
-        self.assertTrue(np.all(2*self.X == esn._scale_inputs(self.X)))
-        esn.fit(self.X,self.y)
+        esn = ESN(N_in, N_out, input_scaling=[2] * N_in)
+        self.assertTrue(np.all(2 * self.X == esn._scale_inputs(self.X)))
+        esn.fit(self.X, self.y)
         esn.predict(self.Xp)
 
-        esn = ESN(N_in,N_out,input_scaling=np.array([2]*N_in))
-        self.assertTrue(np.all(2*self.X == esn._scale_inputs(self.X)))
-        esn.fit(self.X,self.y)
+        esn = ESN(N_in, N_out, input_scaling=np.array([2] * N_in))
+        self.assertTrue(np.all(2 * self.X == esn._scale_inputs(self.X)))
+        esn.fit(self.X, self.y)
         esn.predict(self.Xp)
 
         with self.assertRaises(ValueError):
-            esn = ESN(N_in,N_out,input_scaling=[2]*(N_in+1))
+            esn = ESN(N_in, N_out, input_scaling=[2] * (N_in + 1))
 
         with self.assertRaises(ValueError):
-            esn = ESN(N_in,N_out,input_scaling=np.array([[2]*N_in]))
+            esn = ESN(N_in, N_out, input_scaling=np.array([[2] * N_in]))
 
     def test_inputshift(self):
         """input shift factors of different formats should be correctly interpreted or rejected"""
-        esn = ESN(N_in,N_out,input_shift=1)
-        self.assertTrue(np.all(1+self.X == esn._scale_inputs(self.X)))
-        esn.fit(self.X,self.y)
+        esn = ESN(N_in, N_out, input_shift=1)
+        self.assertTrue(np.all(1 + self.X == esn._scale_inputs(self.X)))
+        esn.fit(self.X, self.y)
         esn.predict(self.Xp)
 
-        esn = ESN(N_in,N_out,input_shift=[1]*N_in)
-        self.assertTrue(np.all(1+self.X == esn._scale_inputs(self.X)))
-        esn.fit(self.X,self.y)
+        esn = ESN(N_in, N_out, input_shift=[1] * N_in)
+        self.assertTrue(np.all(1 + self.X == esn._scale_inputs(self.X)))
+        esn.fit(self.X, self.y)
         esn.predict(self.Xp)
 
-        esn = ESN(N_in,N_out,input_shift=np.array([1]*N_in))
-        self.assertTrue(np.all(1+self.X == esn._scale_inputs(self.X)))
-        esn.fit(self.X,self.y)
+        esn = ESN(N_in, N_out, input_shift=np.array([1] * N_in))
+        self.assertTrue(np.all(1 + self.X == esn._scale_inputs(self.X)))
+        esn.fit(self.X, self.y)
         esn.predict(self.Xp)
 
         with self.assertRaises(ValueError):
-            esn = ESN(N_in,N_out,input_shift=[1]*(N_in+1))
+            esn = ESN(N_in, N_out, input_shift=[1] * (N_in + 1))
 
         with self.assertRaises(ValueError):
-            esn = ESN(N_in,N_out,input_shift=np.array([[1]*N_in]))
+            esn = ESN(N_in, N_out, input_shift=np.array([[1] * N_in]))
 
     def test_IODimensions(self):
         """try different combinations of input & output dimensionalities & teacher forcing"""
-        tasks = [(1,1,100,True),(10,1,100,True),(1,10,100,True),(10,10,100,True),
-                 (1,1,100,False),(10,1,100,False),(1,10,100,False),(10,10,100,False)]
+        tasks = [(1, 1, 100, True), (10, 1, 100, True), (1, 10, 100, True), (10, 10, 100, True),
+                 (1, 1, 100, False), (10, 1, 100, False), (1, 10, 100, False), (10, 10, 100, False)]
         for t in tasks:
-            N_in ,N_out, N_samples, tf = t
-            X = np.random.randn(N_samples,N_in)  if N_in > 1 else np.random.randn(N_samples)
-            y = np.random.randn(N_samples,N_out) if N_out > 1 else np.random.randn(N_samples)
-            Xp = np.random.randn(N_samples,N_in) if N_in > 1 else np.random.randn(N_samples)
-            esn = ESN(N_in,N_out,teacher_forcing=tf)
-            prediction_tr = esn.fit(X,y)
+            N_in, N_out, N_samples, tf = t
+            X = np.random.randn(
+                N_samples, N_in) if N_in > 1 else np.random.randn(N_samples)
+            y = np.random.randn(
+                N_samples, N_out) if N_out > 1 else np.random.randn(N_samples)
+            Xp = np.random.randn(
+                N_samples, N_in) if N_in > 1 else np.random.randn(N_samples)
+            esn = ESN(N_in, N_out, teacher_forcing=tf)
+            prediction_tr = esn.fit(X, y)
             prediction_t = esn.predict(Xp)
-            self.assertEqual(prediction_tr.shape,(N_samples,N_out))
-            self.assertEqual(prediction_t.shape,(N_samples,N_out))
+            self.assertEqual(prediction_tr.shape, (N_samples, N_out))
+            self.assertEqual(prediction_t.shape, (N_samples, N_out))
+
 
 class Performance(unittest.TestCase):
     # Slighty bending the concept of a unit test, I want to catch performance changes during refactoring.
     # Ideally, this will expand to a collection of known tasks.
+
     def test_mackey(self):
         try:
             data = np.load('mackey_glass_t17.npy')
         except IOError:
             self.skipTest("missing data")
 
-        esn = ESN(n_inputs = 1,
-                  n_outputs = 1,
-                  n_reservoir = 500,
-                  spectral_radius = 1.5,
+        esn = ESN(n_inputs=1,
+                  n_outputs=1,
+                  n_reservoir=500,
+                  spectral_radius=1.5,
                   random_state=42)
 
         trainlen = 2000
         future = 2000
-        esn.fit(np.ones(trainlen),data[:trainlen])
+        esn.fit(np.ones(trainlen), data[:trainlen])
         prediction = esn.predict(np.ones(future))
-        error = np.sqrt(np.mean((prediction.flatten() - data[trainlen:trainlen+future])**2))
-        self.assertAlmostEqual(error,0.1396039098653574)
+        error = np.sqrt(
+            np.mean((prediction.flatten() - data[trainlen:trainlen + future])**2))
+        self.assertAlmostEqual(error, 0.1396039098653574)
 
     def test_freqgen(self):
         rng = np.random.RandomState(42)
-        def frequency_generator(N,min_period,max_period,n_changepoints):
+
+        def frequency_generator(N, min_period, max_period, n_changepoints):
             """returns a random step function + a sine wave signal that
                changes its frequency at each such step."""
             # vector of random indices < N, padded with 0 and N at the ends:
-            changepoints = np.insert(np.sort(rng.randint(0,N,n_changepoints)),[0,n_changepoints],[0,N])
-            # list of interval boundaries between which the control sequence should be constant:
-            const_intervals = zip(changepoints,np.roll(changepoints,-1))[:-1]
+            changepoints = np.insert(np.sort(rng.randint(0, N, n_changepoints)), [
+                                     0, n_changepoints], [0, N])
+            # list of interval boundaries between which the control sequence
+            # should be constant:
+            const_intervals = list(
+                zip(changepoints, np.roll(changepoints, -1)))[:-1]
             # populate a control sequence
-            frequency_control = np.zeros((N,1))
-            for (t0,t1) in const_intervals:
+            frequency_control = np.zeros((N, 1))
+            for (t0, t1) in const_intervals:
                 frequency_control[t0:t1] = rng.rand()
-            periods = frequency_control * (max_period - min_period) + max_period
+            periods = frequency_control * \
+                (max_period - min_period) + max_period
 
             # run time through a sine, while changing the period length
-            frequency_output = np.zeros((N,1))
+            frequency_output = np.zeros((N, 1))
             z = 0
             for i in range(N):
                 z = z + 2 * np.pi / periods[i]
-                frequency_output[i] = (np.sin(z) + 1)/2
-            return np.hstack([np.ones((N,1)),1-frequency_control]),frequency_output
+                frequency_output[i] = (np.sin(z) + 1) / 2
+            return np.hstack([np.ones((N, 1)), 1 - frequency_control]), frequency_output
 
         N = 15000
         min_period = 2
         max_period = 10
-        n_changepoints = N/200
-        frequency_control,frequency_output = frequency_generator(N,min_period,max_period,n_changepoints)
+        n_changepoints = int(N / 200)
+        frequency_control, frequency_output = frequency_generator(
+            N, min_period, max_period, n_changepoints)
 
-        traintest_cutoff = np.ceil(0.7*N)
-        train_ctrl,train_output = frequency_control[:traintest_cutoff],frequency_output[:traintest_cutoff]
-        test_ctrl, test_output  = frequency_control[traintest_cutoff:],frequency_output[traintest_cutoff:]
+        traintest_cutoff = int(np.ceil(0.7 * N))
+        train_ctrl, train_output = frequency_control[
+            :traintest_cutoff], frequency_output[:traintest_cutoff]
+        test_ctrl, test_output = frequency_control[
+            traintest_cutoff:], frequency_output[traintest_cutoff:]
 
-        esn = ESN(n_inputs = 2,
-                  n_outputs = 1,
-                  n_reservoir = 200,
-                  spectral_radius = 0.25,
-                  sparsity = 0.95,
-                  noise = 0.001,
-                  input_shift = [0,0],
-                  input_scaling = [0.01, 3],
-                  teacher_scaling = 1.12,
-                  teacher_shift = -0.7,
-                  out_activation = np.tanh,
-                  inverse_out_activation = np.arctanh,
-                  random_state = rng,
-                  silent = True)
+        esn = ESN(n_inputs=2,
+                  n_outputs=1,
+                  n_reservoir=200,
+                  spectral_radius=0.25,
+                  sparsity=0.95,
+                  noise=0.001,
+                  input_shift=[0, 0],
+                  input_scaling=[0.01, 3],
+                  teacher_scaling=1.12,
+                  teacher_shift=-0.7,
+                  out_activation=np.tanh,
+                  inverse_out_activation=np.arctanh,
+                  random_state=rng,
+                  silent=True)
 
-        pred_train = esn.fit(train_ctrl,train_output)
-        #print "test error:"
+        pred_train = esn.fit(train_ctrl, train_output)
+        # print "test error:"
         pred_test = esn.predict(test_ctrl)
         error = np.sqrt(np.mean((pred_test - test_output)**2))
-        self.assertAlmostEqual(error,0.39037112433756577)
+        self.assertAlmostEqual(error, 0.39037112433756577)
 
 
 if __name__ == '__main__':
